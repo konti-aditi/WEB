b1c708a87390902e29f809ca9555ed6f538b8c5d
Aditi <aditis.github@gmail.com>
2026-01-07 18:44:21 +0530

    Var

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+
+
+
+var a = 3390
+var b = "Hydra Sonic"  //variable declaration
+
+
+console.log(a) //printing the var
+console.log(b)
+
+
+document.writeln(b)
+document.writeln("<br>") //brake 
+document.writeln(a) //to display the content on UI
---
+++ JS/var.js
@@ -0,0 +1,9 @@
+
+
+var a;  //declaration
+a = 10; //initialization
+a = 20; //reinitialization
+
+var a = 30;  //reinitialization+declaration
+
+console.log(a);
