a157235bd738840a3bb7b7b72856d1b13e2e12b0
Aditi <aditis.github@gmail.com>
2026-01-08 19:13:41 +0530

    Literals

---
+++ JS/literal.js
@@ -0,0 +1,62 @@
+
+
+// let x = 10
+// console.log(typeof x);
+
+
+let x=true;
+x=nullconsole.log(x);
+
+
+let y;
+console.log(y);
+
+
+
+// let a=Symbol(10)
+// let b=symbol(10)
+// console.log(a==b);
+
+let a=23748950457937456934589374504854394n;
+
+
+let str = "Iam a good girl"
+console.log(str)
+
+let str2 = 'I am a progammer'
+console.log(str2)
+
+let str3 = `I code apps` 
+console.log(str3)
+
+
+
+let sname = "Aditi"
+let place = "Pune"
+let mobile = "1234567892"
+
+
+let info = "My name is " +sname+ "I am from "+place+" and my mobile number is " +mobile
+
+
+
+let info1 = `My name is $(sname) I am from $(place) and my mobile number is $(mobile)`
+
+
+
+let a = 10;
+let b = "10" ; 
+
+console.log(a === b);
+
+
+alert ("Login Successful");
+
+confirm("Are you sure want to exit?")
+
+let x = prompt("Enter a number")
+console.log(x);
+document.writeln(x);
+
+
+
